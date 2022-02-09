522e2727a37fa1c1ff87bb154927e3328a269b88
sonya36 <99344608+sonya36@users.noreply.github.com>
2022-02-09 21:32:08 +0545

    git add.

---
+++ angle.cpp
@@ -0,0 +1,55 @@
+// angle between vector
+
+#include <iostream>
+#include <string>
+using namespace std;
+// class HUmanBeing{
+//  public:
+/// void display(){
+//  cout<<"hello I am Soniya:Your mates";
+// }
+//};
+
+// int main(){
+// HUmanBeing sonia;
+// sonia.dispaly();
+// }
+// class HumanBeing
+// {
+// public:
+//     string name;
+//     void introduce()
+//     {
+//         cout << "hi I am " << name<<endl;
+//     }
+
+// };
+//  int main()
+// {
+//     HumanBeing sonia;
+//     sonia.name = "Soniya";
+//     sonia.introduce();
+//     HumanBeing anmol;
+//     anmol.name = "anmol";
+//     anmol.introduce();
+// }
+// class Human
+// {
+// public:
+//     string name;
+//     void display()
+//     {
+//         cout << "hello I am" << name << ":Your mates";
+//     }
+// };
+
+// int main()
+// {
+//     Human sonia;
+//     sonia.name = "Soniya";
+//     sonia.display();
+//     Human *anmol = new Human();
+//     anmol->name = "Anmol";
+//     anmol->display();
+// }
+
